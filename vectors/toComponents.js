78ca54fb63bde8d6ae391d495dcf7c7d57ec08bc
Kory Nunn <knunn187@gmail.com>
2014-01-11 01:06:07 +1000

    Fixed comments

--- vectors/toComponents.js
+++ vectors/toComponents.js
@@ -1,4 +1,12 @@
-module.exports = function(magnitude, angle /* radians */) {
+/**
+    Vector to Components:
+
+    toComponents(magnatude, angle in degrees)
+
+    returns x,y
+*/
+
+module.exports = function(magnitude, angle) {
     return {
       x: Math.cos(angle) * magnitude,
       y: Math.sin(angle) * magnitude
